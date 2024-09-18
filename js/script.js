99eb4acf55efa0cfe81fc08488bf2ea9d889fdac
Stas Khmylko <metallstas@mail.ru>
2024-09-18 18:21:01 +0300

    add calc

--- js/script.js
+++ js/script.js
@@ -397,4 +397,79 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 
+
+    //Calculation kcal
+
+    const result = document.querySelector('.calculating__result span')
+
+    let sex = 'female'
+    let ratio = 1.375
+
+    let height, weight, age 
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '_______'
+            return
+        }
+
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio)
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio)
+        }
+    }
+
+    calcTotal()
+
+    function getStaticInfo(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`)
+
+        elements.forEach(el => {
+            el.addEventListener('click', e => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio')
+                } else {
+                    sex = e.target.getAttribute('id')
+                }
+    
+                elements.forEach(el => {
+                    el.classList.remove(activeClass)
+                })
+    
+                e.target.classList.add(activeClass)
+                calcTotal()
+    
+            })
+        })
+    }
+
+    getStaticInfo('#gender', 'calculating__choose-item_active')
+    getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active')
+
+    function getDynamicInfo(selector) {
+        const input = document.querySelector(selector)
+        input.addEventListener('input', (e) => {
+            console.log(e.target.value)
+            switch(input.getAttribute('id')) {
+                case 'height': 
+                    height = e.target.value
+                    console.log(height)
+                    break
+                case 'weight': 
+                    weight = e.target.value
+                    break
+                case 'age': 
+                    age = e.target.value
+                    break
+            }
+            calcTotal()
+        })
+
+    }
+
+    getDynamicInfo('#height')
+    getDynamicInfo('#weight')
+    getDynamicInfo('#age')
+
 })
