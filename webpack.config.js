a5e64d0da15b45003f4a7914a2c2021a4479e619
Stas Khmylko <metallstas@mail.ru>
2024-09-25 16:42:46 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,24 @@ module.exports = {
         path: path.resolve(__dirname, './js'),
     },
     watch: true,
+    devtool: "source-map",
+
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: [['@babel/preset-env', {
+                    debug: true,
+                    corejs: 3,
+                    useBuiltIns: "usage"
+                }]]
+              }
+            }
+          }
+        ]
+    }
 }
